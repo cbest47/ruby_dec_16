b5f420765ddb62ad39b12d3021ab6bff95f9deb4
Robert Woodall <rdrwoodall@gmail.com>
2016-11-29 16:01:40 -0600

    Add implementations for assignments from Ruby Fundamentals section.

---
+++ Woodall_Robert/assignments/ruby_fundamentals/my_enumerable.rb
@@ -0,0 +1,13 @@
+module MyEnumerable
+	def my_each
+		for i in self
+			yield i
+		end
+	end
+end
+
+class Array
+	include MyEnumerable
+end
+
+[1,2,3,4].my_each { |i| puts i } #prints 1 2 3 4 in the terminal
