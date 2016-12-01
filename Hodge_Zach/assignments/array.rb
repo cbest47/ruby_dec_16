eb01e71b1dba470674ad3acea8014d0d3ae3a266
amoraq@gmail.com <amoraq@gmail.com>
2016-12-01 06:41:17 -0600

    Playing around with arrays

---
+++ Hodge_Zach/assignments/array.rb
@@ -0,0 +1,13 @@
+a = ["bob", "joe", "chuck", "logan"]
+b = [1, 2, 4, 5, 8]
+
+puts a.at(0)
+puts b.reverse
+puts a.length
+puts a.delete("joe")
+puts b.sort
+puts b.slice(2..4)
+puts b.shuffle
+puts b.join
+puts b.insert(9)
+puts a.values_at(2,3)
