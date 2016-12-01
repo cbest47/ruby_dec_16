942e7ba247e56b15b854ab292e6947fda171a523
amoraq@gmail.com <amoraq@gmail.com>
2016-12-01 06:48:44 -0600

    Playing around with range

---
+++ Hodge_Zach/assignments/range.rb
@@ -0,0 +1,7 @@
+x = (1..10)
+
+puts "Is 11 a member? " + x.member?(11).to_s
+puts "Does it include 5? " + x.include?(5).to_s
+puts "Last in range: " + x.last.to_s
+puts "Max in range: " + x.max.to_s
+puts "Min in range: " + x.min.to_s
