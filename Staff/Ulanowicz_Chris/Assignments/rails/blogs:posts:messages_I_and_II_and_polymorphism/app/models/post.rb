70523b6689fd22840f54afe3e5c71b66f827eab9
Chris Ulanowicz <src3collector@gmail.com>
2016-12-05 09:43:55 -0600

    deleted bugged folders

--- Staff/Ulanowicz_Chris/Assignments/rails/blogs:posts:messages_I_and_II_and_polymorphism/app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :blog
-  has_many :messages, dependent: :destroy
-  has_many :comments, as: :commentable
-  validates :title, :content, presence: true
-  validates :title, length: { minimum: 7 }
-end
